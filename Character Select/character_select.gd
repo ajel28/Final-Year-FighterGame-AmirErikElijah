a836eaa168eb31fdc37ea42e2d4f4c49011769d2
Erik543210 <erikturdiu@gmail.com>
2024-02-28 19:26:01 -0500

    tried to revert again

--- Character Select/character_select.gd
+++ Character Select/character_select.gd
@@ -3,18 +3,15 @@ extends Node2D
 <<<<<<< HEAD
 @export var jinP1select: PackedScene
 @export var jinP2select: PackedScene
-
-
 func _on_jin_button_pressed():
-	while(Global.player<=2):
-		if(Global.player==1):
-			var jin1 = jinP1select.instantiate()
-			get_tree().root.get_node("CharacterSelect").add_child(jin1)
-			Global.player+=1
-		elif(Global.player2==2):
-			var jin2 = jinP2select.instantiate()
-			get_tree().root.get_node("CharacterSelect").add_child(jin2)
-			Global.player+=1
+	if(Global.player%2==1):
+		var jin1 = jinP1select.instantiate()
+		get_tree().root.get_node("CharacterSelect").add_child(jin1)
+		Global.player+=1
+	elif(Global.player%2==0):
+		var jin2 = jinP2select.instantiate()
+		get_tree().root.get_node("CharacterSelect").add_child(jin2)
+		Global.player+=1
 
 func _on_deadpool_button_pressed():
 	pass # Replace with function body.
@@ -49,6 +46,7 @@ func _on_raiden_button_pressed():
 
 func _on_rocky_button_pressed():
 	pass # Replace with function body.
+<<<<<<< HEAD
 
 
 func _on_back_button_pressed():
@@ -77,3 +75,5 @@ func _on_jin_button_pressed():
 func _on_deadpool_button_pressed():
 	pass # Replace with function body.
 >>>>>>> parent of 7424bc8 (continued to work on character selection)
+=======
+>>>>>>> parent of 0fdefc7 (continued)
