0fdefc72cad4e81252085f4b899e709e03553bf1
Erik543210 <erikturdiu@gmail.com>
2024-02-28 19:16:46 -0500

    continued

--- Character Select/character_select.gd
+++ Character Select/character_select.gd
@@ -2,15 +2,18 @@ extends Node2D
 
 @export var jinP1select: PackedScene
 @export var jinP2select: PackedScene
+
+
 func _on_jin_button_pressed():
-	if(Global.player%2==1):
-		var jin1 = jinP1select.instantiate()
-		get_tree().root.get_node("CharacterSelect").add_child(jin1)
-		Global.player+=1
-	elif(Global.player%2==0):
-		var jin2 = jinP2select.instantiate()
-		get_tree().root.get_node("CharacterSelect").add_child(jin2)
-		Global.player+=1
+	while(Global.player<=2):
+		if(Global.player==1):
+			var jin1 = jinP1select.instantiate()
+			get_tree().root.get_node("CharacterSelect").add_child(jin1)
+			Global.player+=1
+		elif(Global.player2==2):
+			var jin2 = jinP2select.instantiate()
+			get_tree().root.get_node("CharacterSelect").add_child(jin2)
+			Global.player+=1
 
 func _on_deadpool_button_pressed():
 	pass # Replace with function body.
@@ -45,3 +48,12 @@ func _on_raiden_button_pressed():
 
 func _on_rocky_button_pressed():
 	pass # Replace with function body.
+
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://Menu/start_menu.tscn")
+
+
+func _on_reset_button_pressed():
+	Global.player=1
+	return true
