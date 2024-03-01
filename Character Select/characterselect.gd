34350546d16a41f99eeadbfa7998eab8729f078c
Erik543210 <erikturdiu@gmail.com>
2024-02-29 19:44:34 -0500

    added DJin player select

--- Character Select/characterselect.gd
+++ Character Select/characterselect.gd
@@ -3,6 +3,9 @@ extends Node2D
 @export var jinP1select: PackedScene
 @export var jinP2select: PackedScene
 
+@export var DjinP1select: PackedScene
+@export var DjinP2select: PackedScene
+
 func _on_jin_button_pressed():
 		if(Global.player==1):
 			var jin1 = jinP1select.instantiate()
@@ -34,7 +37,14 @@ func _on_w_soldier_button_pressed():
 
 
 func _on_d_jin_button_pressed():
-	pass # Replace with function body.
+	if(Global.player==1):
+		var djin1 = DjinP1select.instantiate()
+		get_tree().root.get_node("characterselect").add_child(djin1)
+		Global.player+=1
+	elif(Global.player==2):
+		var djin2 = DjinP2select.instantiate()
+		get_tree().root.get_node("characterselect").add_child(djin2)
+		Global.player+=1
 
 
 func _on_john_wick_button_pressed():
