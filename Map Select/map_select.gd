d65ca76217317d8139b4c78137bd8f8e4f206ca9
Erik543210 <erikturdiu@gmail.com>
2024-03-02 13:55:35 -0500

    did map select screen

--- Map Select/map_select.gd
+++ Map Select/map_select.gd
@@ -18,35 +18,59 @@ extends Node2D
 @export var frostbite: PackedScene
 
 func _on_abandoned_button_pressed():
-	pass # Replace with function body.
+	if(Global.map==0):
+		var abandon = abandonRuins.instantiate()
+		get_tree().root.get_node("MapSelect").add_child(abandon)
+		Global.map+=1
 
 
 func _on_frost_button_pressed():
-	pass # Replace with function body.
+	if(Global.map==0):
+		var frost = frostbite.instantiate()
+		get_tree().root.get_node("MapSelect").add_child(frost)
+		Global.map+=1
 
 
 func _on_into_button_pressed():
-	pass # Replace with function body.
+	if(Global.map==0):
+		var sub = subconscious.instantiate()
+		get_tree().root.get_node("MapSelect").add_child(sub)
+		Global.map+=1
 
 
 func _on_nin_button_pressed():
-	pass # Replace with function body.
+	if(Global.map==0):
+		var nin = ninshitani.instantiate()
+		get_tree().root.get_node("MapSelect").add_child(nin)
+		Global.map+=1
 
 
 func _on_taka_button_pressed():
-	pass # Replace with function body.
+	if(Global.map==0):
+		var taka = takachiho.instantiate()
+		get_tree().root.get_node("MapSelect").add_child(taka)
+		Global.map+=1
 
 
 func _on_depth_button_pressed():
-	pass # Replace with function body.
+	if(Global.map==0):
+		var depth = depths.instantiate()
+		get_tree().root.get_node("MapSelect").add_child(depth)
+		Global.map+=1
 
 
 func _on_dojo_button_pressed():
-	pass # Replace with function body.
+	if(Global.map==0):
+		var dojo = theDojo.instantiate()
+		get_tree().root.get_node("MapSelect").add_child(dojo)
+		Global.map+=1
 
 
 func _on_yaku_button_pressed():
-	pass # Replace with function body.
+	if(Global.map==0):
+		var yaku = yakushima.instantiate()
+		get_tree().root.get_node("MapSelect").add_child(yaku)
+		Global.map+=1
 
 
 func _on_back_button_pressed():
