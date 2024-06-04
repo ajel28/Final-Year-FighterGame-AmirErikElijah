888dbccc6ff1b7fb3b9d8842bab36a3f7ae7dfb3
ajel28 <dolahski123@gmail.com>
2024-06-04 13:26:35 -0400

    health

--- Global Stuff/health_1.gd
+++ Global Stuff/health_1.gd
@@ -1,13 +1,6 @@
-extends Node2D
+extends TextureProgressBar
 class_name H1
 
-@onready var animationsH = $AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
-
-
-func takeHealth1():
-		animationsH.play("damage")
-		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
