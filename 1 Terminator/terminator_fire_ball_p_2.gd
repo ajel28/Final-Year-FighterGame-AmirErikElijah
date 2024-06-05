1140eeda08e6a633fe0be86e2bad7418f1a51750
Erik543210 <156016975+Erik543210@users.noreply.github.com>
2024-06-05 14:59:23 -0400

    fixed bullet catcher

--- 1 Terminator/terminator_fire_ball_p_2.gd
+++ 1 Terminator/terminator_fire_ball_p_2.gd
@@ -8,9 +8,9 @@ func _physics_process(delta):
 	global_position.x += -speed * delta
 		
 func _on_area_entered(area):
-	if (area is RaidenAttP2) or (area is TermFireballPlayer1) or (area is RaidenAttP1) or (area is DoomAttackPlayer1) or (area is DoomAttackPlayer2):
+	if (area is RaidenAttP2) or (area is BulletCatcher) or (area is TermFireballPlayer1) or (area is RaidenAttP1) or (area is DoomAttackPlayer1) or (area is DoomAttackPlayer2):
 		self.queue_free()
 	elif !(area is TermFireballPlayer2):
-		Global.healthp2-=2
+		Global.healthp1-=2
 		print(Global.healthp2)
 		self.queue_free()
