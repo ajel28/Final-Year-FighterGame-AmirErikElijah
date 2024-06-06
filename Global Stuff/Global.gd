3433a416bb017b6934d062bf76c94f71f598e1ae
lemonpeppereel <elijahpark01@gmail.com>
2024-06-06 15:15:56 -0400

    resized maps and added stamina bar

--- Global Stuff/Global.gd
+++ Global Stuff/Global.gd
@@ -11,9 +11,12 @@ var player2
 var mapchosen
 var needHChange = false
 
-
+var staminaP1 = 10
+ 
 var healthp1 = 100
 var maxHealthp1 = 100
+
+var staminaP2 = 100
 
 var maxHealthp2 = 100
 var healthp2 = 100
