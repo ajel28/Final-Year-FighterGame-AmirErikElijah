0032ab04ea237fe5f970177b28a9f537975e617c
ajel28 <dolahski123@gmail.com>
2024-06-11 18:34:03 -0400

    added ryu walking
    idk why my computer wont let me download anything else

--- 1 Ryu/ryu_p_2.gd
+++ 1 Ryu/ryu_p_2.gd
@@ -38,11 +38,15 @@ func get_input():
 	input.x = int(Input.is_action_pressed("ui_L")) - int(Input.is_action_pressed("ui_J"))
 	if int(Input.is_action_pressed("ui_L")) == 1:
 		global_position = global_position.clamp(Vector2(-1550,-999), Vector2(0,750))
+		animationsRyu2.play("ryuwalk2")
+		$TextureRect.flip_h = false
 		punch = false
 		get_node("ShieldP2").hide()
 		return input.normalized()
 	elif int(Input.is_action_pressed("ui_J")) == 1:
 		global_position = global_position.clamp(Vector2(-1550,-999), Vector2(0,750))
+		animationsRyu2.play("ryuwalk2")
+		$TextureRect.flip_h = true
 		punch = false
 		get_node("ShieldP2").hide()
 		return input.normalized()
