13d9b6a57905c942b224a78371c4f99af58b9aaf
Erik543210 <156016975+Erik543210@users.noreply.github.com>
2024-06-12 09:39:08 -0400

    added reload to all character

---
+++ Global Stuff/reload_1doom2.gd
@@ -0,0 +1,21 @@
+extends TextureProgressBar
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	update()
+
+func update():
+	value = $Timer2.time_left*250
+	
+func _input(event):
+	if Input.is_action_pressed("ui_U"):
+		$Timer2.start()
+
+
+func _on_timer_2_timeout():
+	$Timer2.stop()
