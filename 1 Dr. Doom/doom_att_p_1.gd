25d8fd1c407c8058d3e98860eac025913eb9fa7d
Erik543210 <156016975+Erik543210@users.noreply.github.com>
2024-06-12 21:46:22 -0400

    fixed medkit fireball bug

--- 1 Dr. Doom/doom_att_p_1.gd
+++ 1 Dr. Doom/doom_att_p_1.gd
@@ -11,7 +11,7 @@ func _physics_process(delta):
 	global_position.x += speed * delta
 		
 func _on_area_entered(area):
-	if (area is DJinAttack1) or (area is DJinAttack2) or (area is RaidenAttP2) or (area is BulletCatcher) or (area is TermFireballPlayer1) or (area is TermFireballPlayer2) or (area is RaidenAttP1) or (area is DoomAttackPlayer2):
+	if (area is DJinAttack1) or (area is DJinAttack2) or (area is RaidenAttP2) or (area is BulletCatcher) or (area is TermFireballPlayer1) or (area is TermFireballPlayer2) or (area is RaidenAttP1) or (area is DoomAttackPlayer2) or (area is Med):
 		self.queue_free()
 		Global.punchP1 = false
 	elif !(area is DoomAttackPlayer1):
