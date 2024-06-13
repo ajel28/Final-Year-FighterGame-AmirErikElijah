da4e33cf7e117729dfccc26ae24b5f92c56269c4
Erik543210 <156016975+Erik543210@users.noreply.github.com>
2024-06-13 10:03:28 -0400

    Revert "stamina bar"
    This reverts commit 3488407168b909ace57b288ac4e06a09913150f6.

--- Global Stuff/stamina_bar2.gd
+++ Global Stuff/stamina_bar2.gd
@@ -1,43 +1,16 @@
 extends TextureProgressBar
 class_name StaminaBarP2
-@onready var Stamina = $TextureProgressBar
-var can_regen = false
-var time_to_wait = 2
-var s_timer = 0
-var can_start_timer = true
+var Stamina = Timer.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	value = max_value
+	Stamina.set_wait_time(12)
+	Stamina.autostart=true
+	add_child(Stamina)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (can_regen == false && value != 100 or value <= 0):
-		can_start_timer = true
-		if (can_start_timer):
-			s_timer += delta
-			if (s_timer >= time_to_wait):
-				can_regen = true
-				can_start_timer = false
-				s_timer = 0
+	updateStamina()
 
-	if (value < 0):
-		value = 0
-
-	if (value == 100):
-		can_regen = false
-	
-	if (can_regen == true):
-		value += 1
-		can_start_timer = false
-		s_timer = 0
-		
-	if (Input.is_action_just_pressed("ui_I")):
-		value -= 10
-		can_regen = false
-		s_timer = 0
-	
-	if (Input.is_action_just_pressed("ui_K")):
-		value -= 15
-		can_regen = false
-		s_timer = 0
+func updateStamina():
+	value = Stamina.time_left*12.5-50
